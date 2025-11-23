40326f08d2652c28720a282d53c7130742515533
Milos Zivlak <zivlakmilos@gmail.com>
2025-11-23 03:14:10 +0100

    Edit and delete headings

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,8 @@
-import { createEffect, createSignal, For, type Component } from 'solid-js';
+import { For, type Component } from 'solid-js';
 import { generalStore, headingStore, TGeneralStore, weatherStore } from './utils/Storage';
-import { A } from '@solidjs/router';
-import { airplanes, airports, TAirplane } from './utils/Data';
-import { downloadFile, printNavLog } from './utils/Print';
+import { A, useNavigate } from '@solidjs/router';
+import { airplanes, airports } from './utils/Data';
+import { printNavLog } from './utils/Print';
 import { calculateAll } from './utils/Calculations';
 import { loadNavLog, saveNavLog } from './utils/File';
 
@@ -12,6 +12,8 @@ const Home: Component = () => {
   const [generalInfo, setGeneralInfo] = generalStore;
   const [weatherInfo, setWeatherInfo] = weatherStore;
   const [heading, setHeading] = headingStore;
+
+  const navigate = useNavigate();
 
   const fetchWeather = () => {
     const airport = weatherInfo().airport;
@@ -100,6 +102,14 @@ const Home: Component = () => {
 
   const onSaveClicked = () => {
     saveNavLog(generalInfo(), weatherInfo(), heading);
+  }
+
+  const onHeadingEditClicked = (id: number) => {
+    navigate(`/heading/${id}`);
+  }
+
+  const onHeadingDeleteClicked = (id: number) => {
+    setHeading(prev => prev.filter(e => e.id !== id).map((e, idx) => ({ ...e, id: idx })));
   }
 
   const onPrintClicked = async () => {
@@ -224,8 +234,8 @@ const Home: Component = () => {
                     <td class="text-right"></td>
                     <td class="text-right"></td>
                     <td>
-                      <button class="btn btn-success m-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g fill="currentColor"><path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" /><path d="M8 5.5a2.5 2.5 0 1 0 0 5a2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0a3.5 3.5 0 0 1-7 0" /></g></svg></button>
-                      <button class="btn btn-error m-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g fill="currentColor"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" /><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" /></g></svg></button>
+                      <button class="btn btn-success m-1" onClick={() => onHeadingEditClicked(heading.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g fill="currentColor"><path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" /><path d="M8 5.5a2.5 2.5 0 1 0 0 5a2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0a3.5 3.5 0 0 1-7 0" /></g></svg></button>
+                      <button class="btn btn-error m-1" onClick={() => onHeadingDeleteClicked(heading.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g fill="currentColor"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" /><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" /></g></svg></button>
                     </td>
                   </tr>
                 }
